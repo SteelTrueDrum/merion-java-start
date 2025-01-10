5a7e30d8e216501ff4701a8cba938076e9e7fec8
SteelTrueDrum <steeltruedrum@gmail.com>
2025-01-10 17:01:08 +0300

    added Main.java + lesson5

---
+++ src/com/steeltruedrum/java/merion/Main.java
@@ -0,0 +1,9 @@
+package com.steeltruedrum.java.merion;
+
+import com.steeltruedrum.java.merion.lesson5.POJO;
+
+public class Main {
+    public static void main(String[] args) {
+        POJO.run();
+    }
+}
---
+++ src/com/steeltruedrum/java/merion/lesson5/POJO.java
@@ -0,0 +1,37 @@
+package com.steeltruedrum.java.merion.lesson5;
+
+public class POJO {
+
+    public static void run() {
+        // Primitive types
+        byte byteVar = 0x06;
+        short shortVar = 017;
+        int integer = 0;
+        long longVar = 100L;
+        float floatVar = 3.5f;
+        double doubleVar = 10.0;
+        char symbol = 'f';
+        boolean booleanValue = true;
+
+        System.out.println(byteVar);
+        System.out.println(shortVar);
+        System.out.println(integer);
+        System.out.println(longVar);
+        System.out.println(floatVar);
+        System.out.println(doubleVar);
+        System.out.println(symbol);
+        System.out.println(booleanValue);
+
+        String text = "some text";
+        System.out.println(text);
+
+        // Wrapper
+        Integer intVar = 1;
+        Long longVariable = 20L;
+        Double doubleVariable = 10.1;
+
+    }
+
+}
+
+
